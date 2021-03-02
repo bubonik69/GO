4799a3350b70f5d3390c98d63fad900120b26c62
Темка Агентус <C:\Users\agent\AppData\Roaming\The Bat!>
2021-03-03 00:09:56 +0300

    added factorial calculation

--- Lesson1/main.go
+++ Lesson1/main.go
@@ -1,7 +1,23 @@
 package main
+import "fmt"
 
+
+var inputFact int = 5
 func main() {
+	var f int
 	println("Hello word!")
+ for i:=0; i<=inputFact;i++{
+ 	
+	f=f*i
+	 if i==0{
+		 f=1
+	 }
+
+ 	fmt.Println("Факториал",i,"=",f)
+ }
+
+
+
 }
 
 
